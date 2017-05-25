67f28551d58ebc99e382bba4db3b6ce5d7b36255
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-05-25 14:16:23 +0700

    Outline StatusAction tests

---
+++ app/components/Seat/tests/StatusAction.test.js
@@ -0,0 +1,47 @@
+/**
+* Created by jzobro 20170525
+*/
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import StatusAction from '../StatusAction';
+//
+// timerprogress: react.proptypes.number,
+// timerType: React.PropTypes.string, // sitout or action
+
+describe('components.seat.StatusAction', () => {
+  describe('last to act (recent)', () => {
+    describe('should render as 100% opaque', () => {});
+  });
+
+  describe('not last to act (!recent)', () => {
+    describe('should render as 40% opaque', () => {});
+  });
+
+  describe('fold', () => {
+    it('should display info color', () => {});
+  });
+
+  describe('call', () => {
+    it('should display success color', () => {});
+  });
+
+  describe('check', () => {
+    it('should display success color', () => {});
+  });
+
+  describe('all-in', () => {
+    it('should display warning color', () => {});
+  });
+
+  describe('winner', () => {
+    it('should display warning color', () => {});
+  });
+
+  describe('raise', () => {
+    it('should display danger color', () => {});
+  });
+
+  describe('bet', () => {
+    it('should display danger color', () => {});
+  });
+});

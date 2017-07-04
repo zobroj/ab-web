e83773d8f4dcde1329ac7264f5795772c22ea60b
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-07-04 14:53:41 +0700

    Sort by date and type

--- app/containers/Notifications/selectors.js
+++ app/containers/Notifications/selectors.js
@@ -3,12 +3,5 @@ import sortBy from 'lodash/sortBy';
 
 export const selectNotifications = () => createSelector(
   (state) => state.get('notifications'),
-  (notifications) => {
-    const collection = notifications.toJS();
-    if (collection.length > 1) {
-      console.log('sorting');
-      sortBy(collection, ['dismissable', 'date']);
-    }
-    return collection;
-  }
+  (notifications) => sortBy(notifications.toJS(), ['dismissable', 'date']),
 );
